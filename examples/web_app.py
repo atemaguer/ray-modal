7d8372197b1574f4d232dea0e1146b3d9f1fdd26
Atem Aguer <atemjohn@stanford.edu>
2023-09-29 14:52:54 -0700

    web endpoint feature works

--- examples/web_app.py
+++ examples/web_app.py
@@ -1,8 +1,19 @@
+import time
+from fastapi.responses import StreamingResponse
+
 from modal import Stub, web_endpoint
 
 stub = Stub()
 
+def fake_video_streamer():
+    for i in range(10):
+        yield f"frame {i}: some data\n".encode()
+        time.sleep(0.5)
+
+
 @stub.function()
 @web_endpoint()
-def square(x: int):
-    return x**2
+def stream_me():
+    return StreamingResponse(
+        fake_video_streamer(), media_type="text/event-stream"
+    )
